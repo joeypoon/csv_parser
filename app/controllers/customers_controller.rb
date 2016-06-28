ec30b6d797d544801f131a6a5a182e050f081265
Joey F. Poon <joey@joeypoon.com>
2016-06-28 15:31:49 -0500

    Handle bad csv file.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,7 +10,11 @@ class CustomersController < ApplicationController
 
   def create
     @customers = Customer.create!(Customer.from_csv(customer_params[:csv].path))
-    redirect_to root_path
+    redirect_to root_path, notice: "Upload success!"
+  rescue ActiveRecord::RecordInvalid
+    @customer = Customer.new
+    flash[:alert] = "All CSV file entries must have fname, lname, address, city, state, and zip"
+    render :new
   end
 
   private
